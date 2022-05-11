2c7983eb95138f9b1c8200816812d70e5d23fb19
Shahbaz Ikram <ikramshahbaz@gmail.com>
2022-05-11 16:19:37 +0530

    first commit

---
+++ excel.py
@@ -0,0 +1,9 @@
+import xlrd
+xls_path = "C:\\Users\\hp\\Desktop\\excel.xls"
+empty_dict ={}
+sheet = xlrd.open_workbook(xls_path)
+new_sheet = sheet.sheet_by_index(0)
+for i in range (1,new_sheet.nrows):
+    row = new_sheet.row_values(i)
+    empty_dict[row[0]]=row[1:new_sheet.ncols]
+print(empty_dict)
